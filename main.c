f3e7fe21832f006506cd89ad64b2438f3a7a39e7
Kummirai <kummirai@Milton.localdomain>
2022-08-01 21:48:34 +0200

    Create main function

--- main.c
+++ main.c
@@ -1,17 +1,41 @@
-#include "main.h"
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * main - Main Entry
- * Return: 0
+ * main - main function
+ *
+ * Return: 0 (Success)
  */
-
-int read_input(void);
-
-int main(int argc, char **argv)
+int main(void)
 {
-	read_input();
-	char *str_token();
+	/*Two arrays to hold commands: and one array to hold parameters*/
+	char cmd[100], command[100], *parameters[20];
+	char *envp[] = { (char *) "PATH=/bin", 0 };
 
+	while (1)
+	{
+		/*Display prompt*/
+		type_prompt();
+
+		/*read input*/
+		read_command (command, parameters);
+
+		if (fork () != 0)
+			wait (NULL);
+
+		else
+		{
+			strcpy (cmd, "/bin/");
+			strcat (cmd, command);
+			execve (cmd, parameters, envp);
+		}
+		if (strcmp(command, "exit") == 0)
+			break;
+	}
 	return (0);
 }
+
+
