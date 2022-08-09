3b3dd051aaecbdcbfd0c47c1a6062977960ca65b
KwameSei <nathanielosei92@gmail.com>
2022-08-09 21:01:26 +0000

    Main file

--- main.c
+++ main.c
@@ -1,41 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
+#include "main.h"
 
-/**
- * main - main function
- *
- * Return: 0 (Success)
- */
 int main(void)
 {
-	/*Two arrays to hold commands: and one array to hold parameters*/
-	char cmd[100], command[100], *parameters[20];
-	char *envp[] = { (char *) "PATH=/bin", 0 };
+	/* Config files go here */
 
-	while (1)
-	{
-		/*Display prompt*/
-		type_prompt();
+	/* Looping entire program here */
+	program_loop();
 
-		/*read input*/
-		read_command (command, parameters);
+	/* Cleanup / shutdown goes here */
 
-		if (fork () != 0)
-			wait (NULL);
-
-		else
-		{
-			strcpy (cmd, "/bin/");
-			strcat (cmd, command);
-			execve (cmd, parameters, envp);
-		}
-		if (strcmp(command, "exit") == 0)
-			break;
-	}
 	return (0);
 }
-
-
