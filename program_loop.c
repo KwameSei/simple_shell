f3dd563c0715e09058aeb89b635497509bc4ada7
KwameSei <nathanielosei92@gmail.com>
2022-08-09 21:04:17 +0000

    Looping all functions file

---
+++ program_loop.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void program_loop(void)
+{	
+
+	while (1)
+	{
+		prompt();
+	
+/*		arguments = line_into_args(lineread); */
+	/*	execute(); */	
+		read_command(/*par, env*/);
+		/* start_program();*/
+		/*free(lineread);*/
+	/*	free(arguments); */
+	}
+}
