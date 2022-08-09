4ca651c4e739673e7bf9989e417cd6697829ef36
KwameSei <nathanielosei92@gmail.com>
2022-08-09 21:13:52 +0000

    Execute program file

---
+++ execute.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+void execute(void)
+{
+	/*char cmd[] = "/bin/ls";*/
+
+	/*char *argv[] = {NULL};*/
+	/*char *env[] = {NULL};*/
+
+/*	printf("Start of execve call %s:\n", cmd); */ 
+	printf("====================================\n");
+	/*if (execve(cmd, argv, env) == -1)
+	{
+		perror("Could not execute execve");
+	}
+	printf("Oops, something went wrong!");	*/
+}
