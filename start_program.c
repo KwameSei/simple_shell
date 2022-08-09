c53f78d13a65d2cf318ede401788a9b13f53ed9d
KwameSei <nathanielosei92@gmail.com>
2022-08-09 21:12:53 +0000

    Start program

---
+++ start_program.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int start_program(char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		int val = execve(args[0], args, NULL);
+		
+		if (val == -1)
+			perror("Error starting");
+	}
+	else if (pid < 0)
+	{
+	/*	Error forking child process	*/
+		perror("Error");
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
+}
